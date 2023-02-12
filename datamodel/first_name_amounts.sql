7bc16ba66827751fd68bb939143f002c6739ada7
Lassi Valtari <lassi.valtari@outlook.com>
2023-02-13 01:07:30 +0200

    Data model added

---
+++ datamodel/first_name_amounts.sql
@@ -0,0 +1,9 @@
+create table first_name_amounts (
+      id integer primary key
+    , first_name text
+    , gender text
+    , municipality_id integer
+    , year_of_birth integer
+    , amount integer
+    , load_date text
+)
---
+++ datamodel/municipalities.sql
@@ -0,0 +1,15 @@
+create table municipalities (
+      id integer primary key
+    , municipality_number integer
+    , municipality_name_fi text
+    , municipality_name_se text
+    , municipality_type text
+    , primary_language text
+    , electoral_district_number integer
+    , electoral_district_name_fi text
+    , electoral_district_name_se text
+    , region_number integer
+    , region_name_fi text
+    , region_name_se text
+    , load_date text
+)
